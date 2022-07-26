9cd7cb459b3d02b9e08e442b3d0ea60f92741c78
Shaul Friedman <sfriedman@speedata.io>
2022-07-27 02:32:01 +0300

    (+) ignore clang-foramt failed file

--- re-format.sh
+++ re-format.sh
@@ -1,2 +1,2 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-$SCRIPT_DIR/lint-history --relevant 'return filename.endswith((b".c", b".h", b".cpp", b".hpp", b".cxx", b".ixx", b"CMakeLists.txt", b".cmake"))' bash $SCRIPT_DIR/format-code.sh
+$SCRIPT_DIR/lint-history --relevant 'return filename.endswith((b".c", b".h", b".cpp", b".hpp", b".cxx", b".ixx", b"CMakeLists.txt", b".cmake")) and not filename.endswith(b"meta_for.h")' bash $SCRIPT_DIR/format-code.sh
